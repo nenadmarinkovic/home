6c73caf73429ee3726ae3c5023a0f22131c94998
Nenad Marinković <nenadmarinkovic@protonmail.com>
2024-02-28 22:05:49 +0100

    testing grid

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,11 +4,14 @@ import styles from '../styles/components/Banner.module.css';
 
 export default function Banner() {
   return (
-    <div className={styles.banner}>
-      I build <span className={styles.linear}>websites</span>
-      <br /> web interfaces
-      <br />
-      and apps.
-    </div>
+    <>
+      <div className={styles.banner}>
+        I build <span className={styles.linear}>websites</span>
+        <br /> web interfaces
+        <br />
+        and apps.
+      </div>
+      <div className={styles.grid}></div>
+    </>
   );
 }
