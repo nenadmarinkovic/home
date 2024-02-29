88891449fa9a0d8fb4b0d3ee09280965375dbc8c
Nenad Marinković <nenadmarinkovic@protonmail.com>
2024-02-29 20:46:02 +0100

    add banner styles

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 
 import styles from '../styles/components/Banner.module.css';
 import Image from 'next/image';
@@ -7,10 +8,28 @@ export default function Banner() {
   return (
     <>
       <div className={styles.banner}>
-        I build <span className={styles.linear}>websites</span>
-        <br /> web interfaces
-        <br />
-        and apps.
+        <h1 className={styles.banner_text}>
+          <span className={styles.banner_text_top}>
+            I build <span className={styles.linear}>websites</span>,{' '}
+          </span>
+          <span className={styles.banner_text_middle}>
+            web interfaces
+          </span>
+          <span className={styles.banner_text_bottom}>
+            {' '}
+            and apps.
+          </span>
+        </h1>
+        <p className={styles.banner_paragraph}>
+          Front-end developer at nexxar in Vienna, Austria.
+          Experienced in designing and developing websites, web
+          interfaces, and APIs, with a versatile skill set in various
+          web technologies.
+        </p>
+        <div className={styles.triangle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.square}></div>
+        <button className={styles.button}>Contact</button>
       </div>
       <div className={styles.grid}>
         <Image
