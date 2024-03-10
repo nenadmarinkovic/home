efa9a6e8e8f637edc729838100366ba457dc0ecc
Nenad Marinković <nenadmarinkovic@protonmail.com>
2024-03-10 22:07:32 +0100

    add inter fonts, optimize layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,12 @@
 import type { Metadata } from 'next';
-// import localFont from 'next/font/local';
 
 import '../styles/globals.css';
 
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
-
-// const suisseIntl = localFont({
-//   src: [
-//     {
-//       path: '../public/fonts/SuisseIntl-Regular.woff2',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../public/fonts/SuisseIntl-Medium.woff2',
-//       weight: '500',
-//       style: 'normal',
-//     },
-//   ],
-//   variable: '--font-sans-serif',
-// });
-
-// const suisseWorks = localFont({
-//   src: '../public/fonts/SuisseWorks-Regular.woff2',
-//   variable: '--font-serif',
-// });
+const inter = Inter({
+  weight: ['400', '600'],
+});
 
 export const metadata: Metadata = {
   title: 'Nenad Marinković — Frontend Developer',
