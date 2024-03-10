eedf3981c5df6aeee66b8db277833353445faeb1
Nenad Marinković <nenadmarinkovic@protonmail.com>
2024-03-10 21:22:00 +0100

    test fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,32 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+// import localFont from 'next/font/local';
 
 import '../styles/globals.css';
 
-const suisseIntl = localFont({
-  src: [
-    {
-      path: '../public/fonts/SuisseIntl-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/SuisseIntl-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-sans-serif',
-});
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+// const suisseIntl = localFont({
+//   src: [
+//     {
+//       path: '../public/fonts/SuisseIntl-Regular.woff2',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/SuisseIntl-Medium.woff2',
+//       weight: '500',
+//       style: 'normal',
+//     },
+//   ],
+//   variable: '--font-sans-serif',
+// });
+
+// const suisseWorks = localFont({
+//   src: '../public/fonts/SuisseWorks-Regular.woff2',
+//   variable: '--font-serif',
+// });
 
 export const metadata: Metadata = {
   title: 'Nenad Marinković — Frontend Developer',
@@ -31,7 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${suisseIntl.variable}`}>{children}</body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
