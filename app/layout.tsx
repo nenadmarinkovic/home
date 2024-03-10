18a8617ed6f34ec60d2760d1d6f58fe477a70990
Nenad Marinković <nenadmarinkovic@protonmail.com>
2024-03-10 22:45:27 +0100

    fix styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({
-  weight: ['400', '600'],
+  weight: ['400', '500', '600'],
   subsets: ['latin'],
 });
 
