ad15db6325db33c6c2896359e2ee945824bd65b2
Nenad Marinković <nenadmarinkovic@protonmail.com>
2024-04-14 14:29:24 +0200

    add social links

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,10 +2,9 @@
 
 import React, { useState } from 'react';
 import SectionHeader from './Section';
+import Container from '@/containers/Container';
 
 import styles from '../styles/components/Projects.module.css';
-import Container from '@/containers/Container';
-import { link } from 'fs';
 
 function Projects() {
   const [selectedProject, setSelectedProject] = useState(0);
@@ -65,73 +64,69 @@ function Projects() {
     },
   ];
   return (
-    <>
-      <div className={styles.projectContainer}>
-        <Container>
-          <SectionHeader
-            header="Projects"
-            title="Personal web projects including technical details"
-            text="I have worked on a variety of projects, ranging from personal websites to full-stack applications. Here are some of the projects I have worked on."
-          />
-          <div className={styles.flexItems}>
-            <div className={styles.buttons}>
-              {projects.map((project, index) => (
-                <button
-                  className={`${styles.button} ${
-                    selectedProject === index ? styles.selected : ''
-                  }`}
-                  key={index}
-                  onClick={() => {
-                    setSelectedProject(index);
-                    handleClick();
-                  }}
-                >
-                  {project.name}
-                </button>
-              ))}
-            </div>
-            <div
-              className={`${styles.projectDescription} ${styles.textPopup}`}
-              key={key}
-            >
-              <div>
-                <p>{projects[selectedProject].description}</p>
-                <div className={styles.tags}>
-                  {projects[selectedProject].tags.map(
-                    (tag, index) => (
-                      <span className={styles.tag} key={index}>
-                        {tag}
-                      </span>
-                    )
-                  )}
-                </div>
-              </div>
-              <a
-                className={styles.link}
-                target="_blank"
-                href={projects[selectedProject].link}
+    <div className={styles.projectContainer}>
+      <Container>
+        <SectionHeader
+          header="Projects"
+          title="Personal web projects including technical details"
+          text="I have worked on a variety of projects, ranging from personal websites to full-stack applications. Here are some of the projects I have worked on."
+        />
+        <div className={styles.flexItems}>
+          <div className={styles.buttons}>
+            {projects.map((project, index) => (
+              <button
+                className={`${styles.button} ${
+                  selectedProject === index ? styles.selected : ''
+                }`}
+                key={index}
+                onClick={() => {
+                  setSelectedProject(index);
+                  handleClick();
+                }}
               >
-                Visit the page{' '}
-                <svg
-                  className={styles.pageIcon}
-                  height="16"
-                  strokeLinejoin="round"
-                  viewBox="0 0 16 16"
-                  width="16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M5.75001 2H5.00001V3.5H5.75001H11.4393L2.21968 12.7197L1.68935 13.25L2.75001 14.3107L3.28034 13.7803L12.4988 4.56182V10.25V11H13.9988V10.25V3C13.9988 2.44772 13.5511 2 12.9988 2H5.75001Z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-              </a>
-            </div>
+                {project.name}
+              </button>
+            ))}
           </div>
-        </Container>
-      </div>
-    </>
+          <div
+            className={`${styles.projectDescription} ${styles.textPopup}`}
+            key={key}
+          >
+            <div>
+              <p>{projects[selectedProject].description}</p>
+              <div className={styles.tags}>
+                {projects[selectedProject].tags.map((tag, index) => (
+                  <span className={styles.tag} key={index}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <a
+              className={styles.link}
+              target="_blank"
+              href={projects[selectedProject].link}
+            >
+              Visit the page{' '}
+              <svg
+                className={styles.pageIcon}
+                height="16"
+                strokeLinejoin="round"
+                viewBox="0 0 16 16"
+                width="16"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M5.75001 2H5.00001V3.5H5.75001H11.4393L2.21968 12.7197L1.68935 13.25L2.75001 14.3107L3.28034 13.7803L12.4988 4.56182V10.25V11H13.9988V10.25V3C13.9988 2.44772 13.5511 2 12.9988 2H5.75001Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 }
 
