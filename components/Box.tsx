de3d9004fa320117fd9070e77195ea508e3c354a
Nenad Marinković <nenadmarinkovic@protonmail.com>
2024-10-08 22:02:48 +0200

    add observer to home box

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
 import { BoxPropsTypes } from '../types/types';
 import boxStyles from '../styles/components/Box.module.css';
 
@@ -8,8 +11,37 @@ function Box({
   mainTitle,
   boxItems,
 }: BoxPropsTypes) {
+  const [isActive, setIsActive] = useState(false);
+  const boxRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && entry.intersectionRatio === 1) {
+            setIsActive(true);
+          } else {
+            setIsActive(false);
+          }
+        });
+      },
+      { threshold: 1.0 }
+    );
+
+    const currentBoxRef = boxRef.current;
+    if (currentBoxRef) {
+      observer.observe(currentBoxRef);
+    }
+
+    return () => {
+      if (currentBoxRef) {
+        observer.unobserve(currentBoxRef);
+      }
+    };
+  }, []);
+
   return (
-    <div className={boxStyles.boxItem}>
+    <div className={boxStyles.boxItem} ref={boxRef}>
       <div className={boxStyles.boxItemInside}>
         <h3
           className={`${boxStyles.boxItemHeaderTitle} ${boxStyles[boxColor]}`}
@@ -34,9 +66,9 @@ function Box({
         </div>
       </div>
       <div
-        className={`${boxStyles.blur} ${
-          boxStyles[`blur-${boxColor}`]
-        }`}
+        className={`${isActive && boxStyles.active} ${
+          boxStyles.blur
+        } ${boxStyles[`blur-${boxColor}`]} `}
       ></div>
     </div>
   );
