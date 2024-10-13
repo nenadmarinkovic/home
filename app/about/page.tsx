8b11469578c423bef4e450f858861c19ff2c3b97
Nenad Marinković <nenadmarinkovic@protonmail.com>
2024-10-13 19:28:19 +0200

    add profile info

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,7 +20,7 @@ export default function About() {
       <section className={styles.contentContainer}>
         <Container>
           <div className={styles.contentFlex}>
-            <ul className={styles.contentList}>
+            <div className={styles.contentList}>
               <div className={styles.photoContainer}>
                 <Image
                   src="/images/photo.webp"
@@ -30,29 +30,41 @@ export default function About() {
                   className={styles.photo}
                 />
               </div>
-              <svg
-                width="30px"
-                height="30px"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className={styles.quoteIcon}
-              >
-                <rect x="0" fill="none" width="24" height="24" />
-                <g>
-                  <path
-                    fill="#a1e158"
-                    d="M11.192 15.757c0-.88-.23-1.618-.69-2.217-.326-.412-.768-.683-1.327-.812-.55-.128-1.07-.137-1.54-.028-.16-.95.1-1.956.76-3.022.66-1.065 1.515-1.867 2.558-2.403L9.373 5c-.8.396-1.56.898-2.26 1.505-.71.607-1.34 1.305-1.9 2.094s-.98 1.68-1.25 2.69-.346 2.04-.217 3.1c.168 1.4.62 2.52 1.356 3.35.735.84 1.652 1.26 2.748 1.26.965 0 1.766-.29 2.4-.878.628-.576.94-1.365.94-2.368l.002.003zm9.124 0c0-.88-.23-1.618-.69-2.217-.326-.42-.77-.692-1.327-.817-.56-.124-1.074-.13-1.54-.022-.16-.94.09-1.95.75-3.02.66-1.06 1.514-1.86 2.557-2.4L18.49 5c-.8.396-1.555.898-2.26 1.505-.708.607-1.34 1.305-1.894 2.094-.556.79-.97 1.68-1.24 2.69-.273 1-.345 2.04-.217 3.1.165 1.4.615 2.52 1.35 3.35.732.833 1.646 1.25 2.742 1.25.967 0 1.768-.29 2.402-.876.627-.576.942-1.365.942-2.368v.01z"
-                  />
-                </g>
-              </svg>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing
-                elit. Vel nobis sequi illum alias ratione, dignissimos
-                est nostrum temporibus veniam inventore consequatur
-                modi dolorem dicta, unde nesciunt. Repellendus
-                obcaecati dolor nobis.
-              </p>
-            </ul>
+              <ul className={styles.profileInfoItems}>
+                <li className={styles.profileInfoItem}>
+                  <span className={styles.profileInfoValue}>
+                    {new Date().getFullYear() - 2018} years
+                  </span>
+                  <span className={styles.profileInfoKey}>
+                    Experience
+                  </span>
+                </li>
+                <li className={styles.profileInfoItem}>
+                  <span className={styles.profileInfoValue}>
+                    JavaScript, React, Node.js
+                  </span>
+                  <span className={styles.profileInfoKey}>
+                    Core Tech Stack
+                  </span>
+                </li>
+                <li className={styles.profileInfoItem}>
+                  <span className={styles.profileInfoValue}>
+                    Vienna
+                  </span>
+                  <span className={styles.profileInfoKey}>
+                    Location
+                  </span>
+                </li>
+                <li className={styles.profileInfoItem}>
+                  <span className={styles.profileInfoValue}>
+                    English, German, Serbian
+                  </span>
+                  <span className={styles.profileInfoKey}>
+                    Languages
+                  </span>
+                </li>
+              </ul>
+            </div>
             <div className={styles.contentSection}>
               <h3>Some text here</h3>
               <p>
