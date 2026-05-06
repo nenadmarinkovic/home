dc52911fa55e5d110fe07c543bc9d214ef6a38b2
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2026-05-06 14:25:15 +0200

    fix: update site URL assignment to use environment variable

--- lib/site.ts
+++ lib/site.ts
@@ -1,9 +1,11 @@
+const url = process.env.NEXT_PUBLIC_SITE_URL ?? "https://nenadmarinkovic.com";
+
 export const site = {
   name: "Nenad Marinković",
   title: "Nenad Marinković",
   description:
     "Software developer building thoughtful tools and writing about the craft.",
-  url: "https://nenadmarinkovic.com",
+  url,
   author: {
     name: "Nenad Marinković",
     email: "nenadmarinkovic@protonmail.com",
