a46f97cf6263ff42943ecd2eaf65f0e55968ad12
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2026-05-06 16:26:06 +0200

    fix: update site URL assignment to use hardcoded value for preview environment

--- lib/site.ts
+++ lib/site.ts
@@ -1,11 +1,10 @@
-const url = process.env.NEXT_PUBLIC_SITE_URL ?? "https://nenadmarinkovic.com";
-
 export const site = {
   name: "Nenad Marinković",
   title: "Nenad Marinković",
   description:
     "Software developer building thoughtful tools and writing about the craft.",
-  url,
+  // TODO: change to https://nenadmarinkovic.com when deploying to real prod.
+  url: "https://home-preview.monolinie.com",
   author: {
     name: "Nenad Marinković",
     email: "nenadmarinkovic@protonmail.com",
